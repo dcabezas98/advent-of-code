7d335933be4c893c0162f94802c79bfe9c434c1a
David Cabezas <dxabezas@gmail.com>
2018-01-25 16:23:48 +0100

    Fix error

--- download_inputs.py
+++ download_inputs.py
@@ -6,7 +6,7 @@ for day in range(1, 26):
 
     url = ''.join(['http://adventofcode.com/',year,'/day/',str(day),'/input'])
 
-    cookie = {'session':'53616c7465645f5f3e26a2c9638628258749deecbf3ce79b1a2f15c0b6b20d7f1a4bade19854db90de7e414419ff91cb'}
+    cookie = {'session':''}
 
     r = requests.get(url,cookies=cookie)
 
